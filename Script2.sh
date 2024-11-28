a13a6c620c74d7b6202771f8f30541d40f035c85
root <root@ip-172-31-92-119.ec2.internal>
2024-11-28 06:27:57 +0000

    i have added

--- Script2.sh
+++ Script2.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+
+!/bin/bash
 #This script is to read input from the user/console
 echo "Value of a"
 read a
@@ -6,3 +7,4 @@ echo "Value of b"
 read b
 echo "Hello value of a is $a and value of b is $b"
 echo "Do you understand the script?"
+echo "hello everyone"
