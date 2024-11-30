e4a08e1554fa965dceeff523a362a021989b99bd
Sagar Gopale <sagargopale2020@gmail.com>
2024-11-30 17:19:48 +0530

    new enhancement

--- Script2.sh
+++ Script2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #This script is to read input from the user/console
-echo "Value of a"
-read a
 echo "Value of b"
 read b
-echo "Hello value of a is $a and value of b is $b"
-echo "We are performing this practical for batch-20"
+echo "Value of a"
+read a
+echo "Hello value of a is $b and value of b is $a"
+echo "We are performing this practical for batch-21"
