5195e60824a545a7fe671157b413007b35186dac
Sagar Gopale <sagargopale2020@gmail.com>
2024-12-05 21:50:58 +0530

    this is new commit

--- Script2.sh
+++ Script2.sh
@@ -6,3 +6,4 @@ echo "Value of a"
 read a
 echo "Hello value of a is $b and value of b is $a"
 echo "We are performing this practical for batch-21"
+echo "We are performing this practical for batch-18"
