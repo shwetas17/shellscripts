2a15fe5371700f1e1d9ee0e6451d6f377a1c3c57
EC2 Default User <ec2-user@ip-172-31-29-95.ec2.internal>
2024-12-25 14:19:10 +0000

    this is my today's fist commit

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+!/bin/bash
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
+echo "Samiksha"
+echo "Vikas"
 echo "Welcome to Young Minds"
