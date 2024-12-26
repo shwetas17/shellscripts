a61a81589d5c166bfb2ee1d72d8aaddc531a2c7e
Rritesh1234 <141889832+Rritesh1234@users.noreply.github.com>
2024-12-26 17:30:30 +0530

    Update Script4.sh

--- Script4.sh
+++ Script4.sh
@@ -6,4 +6,4 @@ PROFESSION=AWS/DevSecOps/Azure DevOps
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
 echo "we are learning different statergy"
 echo "we r trying to help you to creat conflict"
-echo "this is conflict practical"
+echo "we are facing to create conflict practical"
