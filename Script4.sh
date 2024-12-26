c7fe2ee282bcfa44d2334c1f19c2c34e29cfd045
1023prachu <prachitayade111@gmail.com>
2024-12-26 17:18:37 +0530

    chnges in XXX branch

--- Script4.sh
+++ Script4.sh
@@ -5,3 +5,5 @@ Batch=25
 PROFESSION=AWS/DevSecOps/Azure DevOps
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
 echo "we are learning different statergy"
+echo "we r trying to help you"
+echo "i am from batch24"
